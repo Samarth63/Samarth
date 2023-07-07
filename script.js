3b4fb7f59bbf9ceab987aaf3347a2adae7e6a79e
Samarth63 <137175203+Samarth63@users.noreply.github.com>
2023-07-07 16:26:38 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,30 @@
-var form=document.querySelector('form');
-form.addEventListener('submit',function(e)
+var quoteSource=[{
+    quote:"Start by doing what's necessary; thendo what's possible; andsuddenly you are doing the impossible.", name:"Francis of Assisi"
+},
 {
-    var feet=document.querySelector('#feet');
-    var results=document.querySelector('#results');
-    e.preventDefault();
-    feet= parseInt(feet.value);
-    if(isNaN(feet))
-    {
-        results.textContent="Please enter a valid number:";
-
-    }
-    else if(feet<=0)
-    {
-        results.textContent="Please enter a feet value>0";
-    }
-    else{
-        var converted_value=feet*30.48;
-        results.textContent=converted_value + "cm";
-        document.querySelector(`#feet`).value='';
-        document.querySelector('#inches').value='';
-    }
+    quote:"Believe you can and you're halfway there.",
+    name:"Theodore Roosevelt"
+      
+},
+{
+    quote:"It does not matter how slowly you go aslong as you do not stop.",
+    name:"Confucius"
+},
+{
+  quote:"Our greatest weakness lies in giving up.The most certain way tosucceed is always to try just one more time.",
+  name:"Thomas A. Edison"  
+},
+{
+    quote:"The will to win, the desire to succeed,the urge to reach your fullpotential... these are the keys that will unlock the door to personalexcellence.",
+    name:"Confucius"
 }
-)
+];
+const quoteButton=document.getElementById('quoteButton');
+function myfunction(){
+var sourceLength=quoteSource.length;
+var randomNumber=Math.floor(Math.random()*sourceLength);
+var newQuoteText=quoteSource[randomNumber].quote;
+var newQuoteGenius=quoteSource[randomNumber].name;
+var quoteContainer=document.getElementById('quoteContainer');
+quoteContainer.innerHTML=`<p>${newQuoteText}</p><p id="quoteGenius">-${newQuoteGenius}</p>`;
+};
